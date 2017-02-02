f2ff424348500391029d61f3b30a131bea8cc0d2
Ryan <ryanitt@gmail.com>
2017-02-01 19:16:39 -0700

    More Auto Updates

--- src/org/usfirst/frc/team4627/robot/Robot.java
+++ src/org/usfirst/frc/team4627/robot/Robot.java
@@ -80,7 +80,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
+		autonomousCommand = new ;
 
 		/*
 		 * String autoSelected = SmartDashboard.getString("Auto Selector",
